8fd7cbc524f09539933011e967fc16585177fc5a
Devin Kelly-Collins <dkellycollins@gmail.com>
2012-10-16 19:44:38 -0500

    Added Weapon and Weapon SpriteComponents

---
+++ DungeonCrawler/DungeonCrawler/Components/WeaponComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonCrawler.Components
+{
+    public enum WeaponType
+    {
+        Melee,
+        Ranged,
+    }
+
+    public enum WeaponEffect
+    {
+    }
+
+    public struct Weapon
+    {
+        public uint EntitiyID;
+        public int Damage;
+        public WeaponType Type;
+        public WeaponEffect Effect;
+    }
+
+    class WeaponComponent
+    {
+    }
+}
