78838785f28f37f567ac222fe38515c68bb219b0
Devin Kelly-Collins <dkellycollins@gmail.com>
2012-10-24 18:25:46 -0500

    Making fancy things

--- DungeonCrawler/DungeonCrawler/Systems/CollisionSystem.cs
+++ DungeonCrawler/DungeonCrawler/Systems/CollisionSystem.cs
@@ -9,12 +9,25 @@ namespace DungeonCrawler.Systems
 {
     public class CollisionSystem
     {
+        private enum CollisionType
+        {
+            None = 0x0,
+            Static = 0x1,
+            Player = 0x2,
+            Enemy = 0x4,
+            Bullet = 0x8,
+            PlayerEnemy = 0x6,
+            PlayerBullet = 0xA,
+            PlayerStatic = 0x3,
+            EnemyBullet = 0xC,
+            EnemyStatic = 0x5,
+            BulletStatic = 0x9,
+        }
+
         /// <summary>
         /// The parent game.
         /// </summary>
         private DungeonCrawlerGame _game;
-
-        private List<Collision> _collisions;
 
         /// <summary>
         /// Creates a new collision system.
@@ -35,51 +48,111 @@ namespace DungeonCrawler.Systems
 
             foreach (Player player in _game.PlayerComponent.All)
             {
+                //Get all positions in the room.
                 roomID = _game.PositionComponent[player.EntityID].RoomID;
                 IEnumerable<Position> positionsInRoom = _game.PositionComponent.InRoom(roomID);
 
                 foreach (Position position in positionsInRoom)
                 {
                     IEnumerable<Position> collisions = positionsInRoom.InRegion(position.Center, position.Radius);
-                    if(collisions.Count() > 0)
+                    foreach(Position collidingPosition in collisions)
                     {
-                        foreach(Position collidingPosition in collisions)
+                        CollisionType type = getCollisionType(position.EntityID, collidingPosition.EntityID);
+                        switch (type)
                         {
-                            if(_game.PlayerComponent.Contains(collidingPosition.EntityID) &&
-                                _game.PlayerComponent.Contains(position.EntityID))
-                            {
-                                //Player on Player collision
-                            }
-                            //etc
+                            case CollisionType.Player:
+                                PlayerPlayerCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.PlayerEnemy:
+                                PlayerEnemyCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.PlayerBullet:
+                                PlayerBulletCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.PlayerStatic:
+                                PlayerStaticCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.Enemy:
+                                EnemyEnemyCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.EnemyBullet:
+                                EnemyBulletCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.EnemyStatic:
+                                EnemyStaticCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
+                            case CollisionType.BulletStatic:
+                                BulletStaticCollision(position.EntityID, collidingPosition.EntityID);
+                                break;
                         }
                     }
                 }
             }
         }
 
-        private void findCollisions(IEnumerable<Position> positions, Vector2 center, float radius)
+        private void BulletStaticCollision(uint p, uint p_2)
         {
-            IEnumerable<Position> positionsInRegion = positions.InRegion(center, radius);
-        }
-    }
-
-    public class Collision
-    {
-        public uint Entity1ID;
-        public uint Entity2ID;
-
-        public Collision(uint e1, uint e2)
-        {
-            Entity1ID = e1;
-            Entity2ID = e2;
+            throw new NotImplementedException();
         }
 
-        public override bool Equals(object obj)
+        private void EnemyStaticCollision(uint p, uint p_2)
         {
-            if (obj is Collision)
-                return (obj as Collision).Entity1ID == this.Entity1ID && (obj as Collision).Entity2ID == this.Entity2ID;
-            else
-                return false;
+            throw new NotImplementedException();
+        }
+
+        private void EnemyBulletCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EnemyEnemyCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerStaticCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerBulletCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerEnemyCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerPlayerCollision(uint p, uint p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private CollisionType getCollisionType(uint p, uint p_2)
+        {
+            CollisionType obj1 = CollisionType.None;
+            if (_game.PlayerComponent.Contains(p))
+                obj1 = CollisionType.Player;
+            else if (false) //Enemy
+                obj1 = CollisionType.Enemy;
+            else if (_game.BulletComponent.Contains(p))
+                obj1 = CollisionType.Bullet;
+            else if (false) //Static
+                obj1 = CollisionType.Static;
+
+            CollisionType obj2 = CollisionType.None;
+            if (_game.PlayerComponent.Contains(p))
+                obj2 = CollisionType.Player;
+            else if (false) //Enemy
+                obj2 = CollisionType.Enemy;
+            else if (_game.BulletComponent.Contains(p))
+                obj2 = CollisionType.Bullet;
+            else if (false) //Static
+                obj2 = CollisionType.Static;
+
+            return obj1 | obj2;  
         }
     }
 }
