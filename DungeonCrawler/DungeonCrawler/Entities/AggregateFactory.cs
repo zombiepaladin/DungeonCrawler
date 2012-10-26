1c2a5e259ad1d72aef321404cb6d5e6a6df6f745
Devin Kelly-Collins <dkellycollins@gmail.com>
2012-10-26 11:16:14 -0500

    Added/Updated file descriptions

--- DungeonCrawler/DungeonCrawler/Entities/AggregateFactory.cs
+++ DungeonCrawler/DungeonCrawler/Entities/AggregateFactory.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// AggregateFactory.cs 
+//
+// Author: 
+//
+// Modified: Devin Kelly-Collins, Factory methods return eid, 10/24/2012
+//
+// Kansas State Univerisity CIS 580 Fall 2012 Dungeon Crawler Game
+// Copyright (C) CIS 580 Fall 2012 Class. All rights reserved.
+// Released under the Microsoft Permissive Licence 
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
