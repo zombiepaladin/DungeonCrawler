6a02b3100ef52dc53b34e54b9b7d008c7cc3b4f0
unknown <DevinKC@n122w71.w2k.cis.ksu.edu>
2012-10-26 11:02:28 -0500

    Added collision compoenet and bounds classes

---
+++ DungeonCrawler/DungeonCrawler/Bounds/CircleBounds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace DungeonCrawler
+{
+    public class CircleBounds : Bounds
+    {
+        public Vector2 Center;
+        public float Radius;
+
+        public CircleBounds(Vector2 center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public bool Intersect(Bounds obj)
+        {
+            if (obj is CircleBounds)
+                return handleCircleIntersect(obj as CircleBounds);
+            else
+                return handleRectangleIntersect(obj as RectangleBounds);
+        }
+
+        private bool handleCircleIntersect(CircleBounds obj)
+        {
+            return false;
+        }
+
+        private bool handleRectangleIntersect(RectangleBounds obj)
+        {
+            return false;
+        }
+    }
+}
---
+++ DungeonCrawler/DungeonCrawler/Components/CollisionComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonCrawler.Components
+{
+    public struct Colliedable
+    {
+        public uint EntityID;
+
+        public Bounds Bounds;
+    }
+
+    public class CollisionComponent : GameComponent<Colliedable>
+    {
+
+    }
+}
