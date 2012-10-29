45c31143be402b4df14a3b3b074c368f8618567d
unknown <malinajirka@n122w77.w2k.cis.ksu.edu>
2012-10-29 10:43:09 -0500

    comments added

--- DungeonCrawler/DungeonCrawler/Components/TriggerComponent.cs
+++ DungeonCrawler/DungeonCrawler/Components/TriggerComponent.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+// TriggerComponent.cs
+//
+// Author: Samuel Fike and Jiri Malina
+//
+// Kansas State Univerisity CIS 580 Fall 2012 Dungeon Crawler Game
+// Copyright (C) CIS 580 Fall 2012 Class. All rights reserved.
+// Released under the Microsoft Permissive Licence 
+//-----------------------------------------------------------------------------
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
