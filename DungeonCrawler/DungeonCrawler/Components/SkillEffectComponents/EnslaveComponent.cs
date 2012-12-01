0233f86fbef505ddaf36c9af31d9e36598fa0a2e
Versaiteis <nickaboen+GitHub@gmail.com>
2012-12-01 14:18:20 -0600

    Had a bit of an issue with a missing file, didn't include EnslaveComponent.cs, so here it is

---
+++ DungeonCrawler/DungeonCrawler/Components/SkillEffectComponents/EnslaveComponent.cs
@@ -0,0 +1,30 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// StunComponent.cs 
+//
+// Author: Nicholas Boen
+// 
+// Kansas State Univerisity CIS 580 Fall 2012 Dungeon Crawler Game
+// Copyright (C) CIS 580 Fall 2012 Class. All rights reserved.
+// Released under the Microsoft Permissive Licence 
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonCrawler.Components
+{
+    public struct Enslave
+    {
+        public uint EntityID;
+        public uint OwnerID;
+        public uint TargetID;
+    }
+
+    public class EnslaveComponent : GameComponent<Enslave>
+    {
+    }
+}
